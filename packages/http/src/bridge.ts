bcc07c2c08d62cadda2f003584ec3cd4b7663132
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-10-28 01:58:58 -0400

    sync

--- packages/http/src/bridge.ts
+++ packages/http/src/bridge.ts
@@ -1,29 +1,40 @@
+import type {} from "statedrive";
+
 import { Bridge } from "@hydrophobefireman/flask-jwt-jskit";
 import { _util } from "@hydrophobefireman/kit";
 import { redirect } from "@hydrophobefireman/ui-lib";
 
-const client = new Bridge<any>(null);
+export function buildHttpClient({
+  loginEndPoint,
+  refreshTokenEndPoint,
+  initialAuthCheckEndPoint,
+  loginRoute,
+}: {
+  loginEndPoint?: string;
+  refreshTokenEndPoint?: string;
+  initialAuthCheckEndPoint?: string;
+  loginRoute?: string;
+}) {
+  const client = new Bridge<any>(null);
 
-// change these according to your backend
-client.setRoutes({
-  loginRoute:
-    process.env.KIT_API_LOGIN_ROUTE ||
-    _util.warn("/api/login", "Using default Login Route"),
-  refreshTokenRoute:
-    process.env.KIT_API_REFRESH_ROUTE ||
-    _util.warn("/api/refresh", "Using default Refresh Route"),
-  initialAuthCheckRoute:
-    process.env.KIT_API_AUTH_CHECK_ROUTE ||
-    _util.warn("/api/users/me", "Using default Auth check route"),
-});
-client.onLogout(() =>
-  redirect(
-    process.env.LOGIN_PAGE || _util.warn("/login", "Using default Login Page")
-  )
-);
+  // change these according to your backend
+  client.setRoutes({
+    loginRoute:
+      loginEndPoint || _util.warn("/api/login", "Using default Login Route"),
+    refreshTokenRoute:
+      refreshTokenEndPoint ||
+      _util.warn("/api/refresh", "Using default Refresh Route"),
+    initialAuthCheckRoute:
+      initialAuthCheckEndPoint ||
+      _util.warn("/api/users/me", "Using default Auth check route"),
+  });
+  client.onLogout(() =>
+    redirect(loginRoute || _util.warn("/login", "Using default Login Page"))
+  );
 
-const { useAuthState, useIsLoggedIn } = client.getHooks();
+  const { useAuthState, useIsLoggedIn } = client.getHooks();
 
-const requests = client.getHttpClient();
+  const requests = client.getHttpClient();
 
-export { useAuthState, useIsLoggedIn, requests, client };
+  return { useAuthState, useIsLoggedIn, requests, client };
+}
