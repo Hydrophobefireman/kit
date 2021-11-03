1d00b5d00d330e581b99839b48220d731d0f526a
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-11-03 18:35:10 -0400

    fix

--- packages/hooks/src/use-scroll.ts
+++ packages/hooks/src/use-scroll.ts
@@ -1,11 +1,14 @@
-import {useLatestRef} from "./use-latest-ref";
-import {useMount} from "./use-mount";
+import { useLatestRef } from "./use-latest-ref";
+import { useMount } from "./use-mount";
 
-export function useScroll(cb: (e: Event) => void) {
+export function useScroll(
+  cb: (e: Event) => void,
+  options?: AddEventListenerOptions
+) {
   const ref = useLatestRef(cb);
   useMount(() => {
     const callback = (e: Event) => ref.current(e);
-    window.addEventListener("scroll", callback);
+    window.addEventListener("scroll", callback, options || {});
     return () => window.removeEventListener("scroll", callback);
   });
 }
